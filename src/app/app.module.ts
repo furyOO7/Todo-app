8f1e8ab085d3e6dcc265489be35d7384b139ca45
Adarsh Yadav <AdarshY@ILMUX.com>
2018-12-19 15:09:14 +0530

    chnages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,FormsModule,ModalModule.forRoot(), HttpClientModule
+    BrowserModule,FormsModule,ModalModule.forRoot(), HttpClientModule, Angular2FontawesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
