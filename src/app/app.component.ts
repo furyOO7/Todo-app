aafa89b25a735990371754350c1437972ed2ebf1
Adarsh Yadav <AdarshY@ILMUX.com>
2018-12-19 12:35:23 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component ,  OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
+
 
 
 
@@ -17,13 +19,13 @@ export class AppComponent implements OnInit{
   updatedTodo: any;
   title = 'Todo';
   updatedlabel = 'Test'; 
+ 
    
    constructor(public _http: HttpClient){
      this.todos = []
      this.data = {}
      this.clickedTodo = {}
      this.updatedTodo = "";
-
    };
 
   loadTodo() {
@@ -32,9 +34,11 @@ export class AppComponent implements OnInit{
         'Content-type': "application/json"
       }
     }).subscribe(response => {
-      //console.log("response", response)
-      if(response.success){
-         this.todos = response.data;
+      // console.log("response", response)
+      let res = JSON.parse(JSON.stringify(response))
+       console.log("res", res)
+      if(res.success){
+         this.todos = res.data;
       }
       console.log("this.todos", this.todos)
     })
@@ -44,17 +48,18 @@ export class AppComponent implements OnInit{
    
   }
   addTodo(newTodo) {
-    console.log(newTodo.value)
+    console.log(newTodo)
     let data =  {
-  "todo_name": newTodo.value,
-  "status": false
+          "todo_name": newTodo,
+          "status": false
   }
 this._http.post("http://localhost:3000/todo/add", data, {
   headers: {
     'Content-type': 'application/json'}}
     ).subscribe(response => {
     console.log("response", response);
-    if(response.success){
+     let res = JSON.parse(JSON.stringify(response))
+    if(res.success){
       this.loadTodo()
     }else{
       console.log("error")
@@ -72,7 +77,8 @@ this._http.post("http://localhost:3000/todo/add", data, {
   headers: {
     'Content-type': 'application/json'}}
     ).subscribe(response => {
-    if(response.success){
+      let res = JSON.parse(JSON.stringify(response))
+    if(res.success){
       console.log(response)
       todo.status = !todo.status
     }else{
@@ -104,19 +110,39 @@ this._http.post("http://localhost:3000/todo/add", data, {
     'Content-type': "application/json"
   }
 }).subscribe(response => {
-  if(response.success){
+  let res = JSON.parse(JSON.stringify(response))
+  if(res.success){
     this.loadTodo();
      $('#exampleModal').modal('hide');
      this.updatedTodo = "";
   }
 })
-
   }
- /* this.clickedTodo.name = updatedTodo;*/
-
     else{
       alert("Kindly enter something")
     }
   
+  }
+   confirmDeleteTodo(todo){
+    $("#deleteModal").modal('show');
+    this.todoDelete = todo;
+  }
+  deleteTodo(){
+    let data = {
+      "id": this.todoDelete.id,
+      "name": this.todoDelete.todo_name
+    };
+    this._http.post("http://localhost:3000/todo/delete/" + this.todoDelete.id, data, {
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }).subscribe(response => {
+      if(response.success){
+        console.log("response", response)
+         $("#deleteModal").modal('hide');
+        this.loadTodo();
+      }
+      
+    })
   }
 }
